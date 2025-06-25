816f068068713380db638fd3a5b7f6d00224abcf
grovince <grovinceln@gmail.com>
2025-06-26 00:10:45 +0900

    [Feat]: FastAPI 환경 구축

---
+++ fastapi_project/app/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
