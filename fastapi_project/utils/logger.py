1f9674e83b990e92d7bf8748426b3e6b42bccc9c
grovince <grovinceln@gmail.com>
2025-07-03 22:53:39 +0900

    [Feat]: access 관련 로그 함수 추가

--- fastapi_project/utils/logger.py
+++ fastapi_project/utils/logger.py
@@ -1,7 +1,8 @@
 import logging
 import os
+from pythonjsonlogger import jsonlogger
 
-def get_logger():
+def get_auth_logger():
     logger = logging.getLogger("auth_logger")
     logger.setLevel(logging.INFO)
     log_path = "D:/project/ELK-Track/fastapi_project/log/auth.log"
@@ -9,6 +10,20 @@ def get_logger():
     
     if not logger.handlers:
         handler = logging.FileHandler(log_path, mode="a", encoding="utf-8")
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s') # json 형식으로 로그 변환 
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+
+def get_access_logger():
+    logger = logging.getLogger("access_logger")
+    logger.setLevel(logging.INFO)
+    log_path = "D:/project/ELK-Track/fastapi_project/log/access.log"
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    
+    if not logger.handlers:
+        handler = logging.FileHandler(log_path, mode="a", encoding="utf-8")
+        formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
